c6af291d1b91283e1545ea8398f4c12617b88ec9
Jose Lara <larazjoseenrique74@gmail.com>
2025-02-27 19:36:51 -0600

    Create Creditos.py

---
+++ Python/Creditos.py
@@ -0,0 +1,13 @@
+
+# Solicitar al usuario ingresar un número
+numero = int(input("Ingresa el número de creditos obtenidos: ")) 
+
+# Verificar si tiene los creditos academicos necesarios 
+if numero >= 80 <= 119 :
+    print("Puedes hacer servicio social.")
+else:
+    print("No puedes hacer servicio social.") 
+if numero >= 120 :
+    print("Puedes hacer recidencia profecional.")
+else:
+    print("No puedes hacer recidencia profecional.") 
