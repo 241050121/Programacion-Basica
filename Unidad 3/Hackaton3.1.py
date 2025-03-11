31e91009338cf961b91ac496f38326364002c597
Jose Lara <larazjoseenrique74@gmail.com>
2025-03-10 21:03:14 -0600

    Carpetas

---
+++ Unidad 3/Hackaton3.1.py
@@ -0,0 +1,7 @@
+def realizar_mandado(): 
+# Preguntar si desea realizar el mandado
+    respuesta = (str (input("¿Deseas realizar el mandado? (si/no): ")))
+
+    if respuesta != 'si':
+        print("Gracias por usar el programa. ¡Hasta luego!")
+    return
