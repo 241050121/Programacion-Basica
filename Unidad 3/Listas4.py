acb9b077ca22bf16125b68e0bca537a6813737c7
Jose Lara <larazjoseenrique74@gmail.com>
2025-03-11 08:02:04 -0600

    Listas 4 incompleto

---
+++ Unidad 3/Listas4.py
@@ -0,0 +1,22 @@
+# Lista de cadenas
+nombres = ["Aaron", "Oswaldo", "Roberto", "Adan", "Manuel","Alejandro", "Cristian", "Juan", "Karol","Guadalupe", "Jesus"]
+
+# Lista de números
+edades = [18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18]
+
+#Razones para jugar Videojuegos
+razon = ["Te diviertes","Te desestresas","Pasas el tiempo","Convives con amigos","Realizan un hobbie","Haces enojar a gente random","Descargas tu estres en el juego","Peleas con gente","Mejores tus habilidades","Pasas el tiempo"]
+
+# Lista de cadenas
+materias = ["Programacion basica","Calculo integral","Administracion de empresas","Ingles","Ing de los materiales","Algebra lineal","Contabilidad"]
+
+# Lista de cadenas
+profesores = ["Flores","La risitas","La gallos","Finish class","El presumido","El ASMR", "Abuelo de Manuel"]
+
+# Imprimir listas 
+materias.remove("Programacion basica")
+print(materias)
+materias.append("Programacion Avanzada")
+print(materias)
+materias.append([1] "Programacion Basica")
+print(materias)
